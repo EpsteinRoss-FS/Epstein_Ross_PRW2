964abe30b16ad23b3791c997449abd982bf83e2a
EpsteinRoss-FS <74941767+EpsteinRoss-FS@users.noreply.github.com>
2022-01-28 14:06:56 -0500

    button modifies chart

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -3,6 +3,7 @@ import {LineChart, Line, XAxis, YAxis, CartesianGrid} from 'recharts';
 import React, {useState} from "react";
 import Button from 'react-bootstrap/Button';
 import {Tooltip} from "react-bootstrap";
+import produce from "immer";
 
 ;
 
@@ -11,7 +12,11 @@ class Reports extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 10
+            count: 10,
+            data: [
+                {name: 'Page A', uv: 0, pv: 2400, amt: 2400},
+                {name: 'Page B', uv: 1, pv: 2400, amt: 2400},
+            ]
         };
     }
 
@@ -19,7 +24,7 @@ class Reports extends React.Component {
         return (
             <div>
 
-                <LineChart width={600} height={300} data={this.state.count} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                <LineChart width={600} height={300} data={this.state.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
                     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                     <XAxis dataKey="name"    />
@@ -27,7 +32,12 @@ class Reports extends React.Component {
                     <Tooltip />
                 </LineChart>
 
-                <Button onClick={() => this.setState({ count: this.state.count + 1 })}>
+                <p>Data {this.state.data[1].uv} times</p>
+                <p>You clicked {this.state.count} times</p>
+                <Button onClick={() =>this.setState(produce(state => {
+                    state.data[1].uv = this.state.count + 1;
+                    state.count = this.state.count + 1;
+                    }))}>
                     Click me
                 </Button>
             </div>
